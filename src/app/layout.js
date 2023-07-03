c52f3fb5ad32d193e34a1ad0777126f7c68d161e
Mersharch <mersharchdemenya@icloud.com>
2023-07-03 11:25:11 +0000

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,8 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className= 'bg-white flex flex-col h-screen w-screen sm:bg-blue-400 md:bg-yellow-200 lg:bg-red-400 xl:bg-green-400'
+        className= 'bg-white flex flex-col h-screen w-screen '
       >
+        {/* sm:bg-blue-400 md:bg-yellow-200 lg:bg-red-400 xl:bg-green-400 */}
         <AuthProvider>
           <Navbar />
           {children}
